1bfbf61fde4f81994fd88db349cfa844e2b1a78e
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-12 07:54:02 -0700

    How to make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    SUM = len(sys.argv)
+    if SUM <= 1:
+        print("0 arguments.")
+    else:
+        if SUM == 2:
+            print("{:d} argument:".format(SUM - 1))
+        else:
+            print("{:d} arguments:".format(SUM - 1))
+        for i in range(1, SUM):
+            print("{:d}: {}".format(i, sys.argv[x]))
