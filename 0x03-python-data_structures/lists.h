92f13d5ad3fd72c403b964bacd00a28f75f503de
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-14 14:09:34 -0700

    lists

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @x: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int x;
+	struct listint_s *task;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int x);
+void free_listint(listint_t *head);
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
