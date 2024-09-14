d0c9a430ad292fcbeb61f45c4bb8b9bc1aa29806
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-14 12:48:36 -0700

    Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,6 +7,6 @@ def max_integer(my_list=[]):
     maxi = my_list[0]
     for x in range(len(my_list)):
         if my_list[x] > maxi:
-            maxi = my_list[i]
+            maxi = my_list[x]
 
     return (maxi)
