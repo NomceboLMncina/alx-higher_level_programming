cb0bb25decd5ac666a68f127f59493ffb85717b1
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-16 05:28:48 -0700

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,11 +8,11 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	int elem;
+	int x;
 
 	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (elem = 0; elem < Py_SIZE(p); elem++)
-		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
+	for (x = 0; x < Py_SIZE(p); x++)
+		printf("Element %d: %s\n", x, Py_TYPE(PyList_GetItem(p, x))->tp_name);
 }
 
