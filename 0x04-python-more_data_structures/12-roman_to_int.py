954dfb1472add361273d8b389d59dcd7bd9c38d2
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-18 16:50:24 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,12 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    total = 0
-    previous_value = 0
-
-    for char in roman_string:
-        current_value = roman_dict[char]
-
-        if current_value > previous_value:
-            total += current_value - 2 * previous_value
+    r_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r_n = 0
+    for x in range(len(roman_string)):
+        if x > 0 and r_d[roman_string[x]] > r_d[roman_string[x - 1]]:
+            r_n += r_d[roman_string[x]] - 2 * r_d[roman_string[x - 1]]
         else:
-            total += current_value
-
-            previous_value = current_value
-
-    return total
+            r_n += r_d[roman_string[x]]
+    return r_n
