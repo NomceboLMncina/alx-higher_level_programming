ac0796f740c635d5bf9d8d92b35e7b3601b87eee
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-18 10:53:39 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     r_i = 0
     for x in range(len(roman_string)):
         if x > 0 and rd[roman_string[x]] > rd[roman_string[x - 1]]:
-            r_i += rd[roman_string[x]] - 2 * \
+            r_i += rd[roman_string[x]] - 2 * 1
                         rd[roman_string[x - 1]]
         else:
             r_i += rd[roman_string[x]]
