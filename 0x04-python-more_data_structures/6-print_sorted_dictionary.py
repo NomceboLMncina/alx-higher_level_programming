f98392cac1a676a486b5c67cf36d58ea7b992c21
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-18 09:52:16 -0700

    Print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(dictionary):
-    for x in sorted(dictionary):
-        print("{:s}: {}".format(x, dictionary[x]))
+def print_sorted_dictionary(a_dictionary):
+    for x in sorted(a_dictionary):
+        print("{:s}: {}".format(x, a_dictionary[x]))
