3dc8bcac15b3904c77fb43c33426efe635f060c2
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-18 11:09:02 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
-    rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = [data[x] for x in roman_string] + [0]
     r_i = 0
-    for x in range(len(roman_string)):
-        if x > 0 and rd[roman_string[x]] > rd[roman_string[x - 1]]:
-            r_i += rd[roman_string[x]] - 2 * 1
-                        rd[roman_string[x - 1]]
+
+    for i in range(len(num) - 1):
+        if num[y] >= num[y+1]:
+            r_i += num[y]
         else:
-            r_i += rd[roman_string[x]]
+            r_i -= num[y]
+
     return r_i
