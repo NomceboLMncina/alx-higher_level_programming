7901daacc117606db2ce190844b836c649ef6524
Nomcebo Lunga Mncina <nomcebolungapearlmncina@gmail.com>
2024-09-30 13:42:40 -0700

    CPython #2: PyFloatObject

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,49 +1,44 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
-#include <bytesobject.h>
-#include <floatobject.h>
-#include <stdlib.h>
-#include <float.h>
+#include <Python.h> 
+#include <object.h> 
+#include <listobject.h> 
+#include <bytesobject.h> 
+#include <floatobject.h> 
+#include <stdlib.h> 
+#include <float.h> 
+void print_python_float(PyObject *p) 
+{ 
+PyFloatObject *f = (PyFloatObject *)p; 
+double d = f->ob_fval; 
+char *str = NULL; 
+printf("[.] float object info\n"); 
+if (!PyFloat_Check(f)) 
+{ 
+printf(" [ERROR] Invalid Float Object\n"); 
+return; 
+} 
+str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL); 
+printf(" value: %s\n", str); 
+} 
+void print_python_bytes(PyObject *p) 
+{ 
+long unsigned int s; 
+unsigned int x; 
+char *trying_str = NULL; 
+printf("[.] bytes object info\n"); 
+if (!PyBytes_Check(p)) 
+{ 
+printf(" [ERROR] Invalid Bytes Object\n"); 
+return; 
+} 
+s = ((PyVarObject *)p)->ob_size; 
+trying_str = ((PyBytesObject *)p)->ob_sval; 
+printf(" s: %lu\n", s); 
+printf(" trying string: %s\n", trying_str); 
+if (s < 10) 
+printf(" first %lu bytes:", s + 1); 
+else
 
-void print_python_float(PyObject *p)
-{
-    PyFloatObject *f = (PyFloatObject *)p;
-    double d = f->ob_fval;
-    char *str = NULL;
-
-    printf("[.] float object info\n");
-    if (!PyFloat_Check(f))
-    {
-        printf("  [ERROR] Invalid Float Object\n");
-        return;
-    }
-    
-    str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-    printf("  value: %s\n", str);
-}
-
-void print_python_bytes(PyObject *p)
-{
-    long unsigned int s;
-    unsigned int x;
-    char *trying_str = NULL;
-
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
-
-    s = ((PyVarObject *)p)->ob_size;
-    trying_str = ((PyBytesObject *)p)->ob_sval;
-    printf("  s: %lu\n", s);
-    printf("  trying string: %s\n", trying_str);
-    if (s < 10)
-        printf("  first %lu bytes:", s + 1);
-    else
-        printf("  first 10 bytes:");
+printf("  first 10 bytes:");
     for (x = 0; x <= s && x < 10; x++)
         printf(" %02hhx", trying_str[x]);
     printf("\n");
@@ -55,7 +50,6 @@ void print_python_list(PyObject *p)
     unsigned int x;
     PyListObject *list = (PyListObject *)p;
     const char *type;
-
     printf("[*] Python list info\n");
     if (!PyList_Check(list))
     {
@@ -76,3 +70,4 @@ void print_python_list(PyObject *p)
             print_python_float(list->ob_item[x]);
     }
 }
+
