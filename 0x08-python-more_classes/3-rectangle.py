9166b9dfdcfdfbef150b9ad3cba45060df201365
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-10-07 10:29:55 -0700

    A&P

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
+
         Args:
             width: width of the rectangle
             height: height of the rectangle
@@ -22,8 +23,8 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ''
         rec_str = ''
-        for x in range(self.__height):
-            for y in range(self.__width):
+        for i in range(self.__height):
+            for j in range(self.__width):
                 rec_str += '#'
             rec_str += '\n'
         return rec_str[:-1]
@@ -36,6 +37,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the width of a Rectangle instance
+
         Args:
             value: value of the width, must be a positive integer
         """
@@ -53,6 +55,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of a Rectangle instance
+
         Args:
             value: value of the height, must be a positive integer
         """
@@ -64,6 +67,7 @@ class Rectangle:
 
     def area(self):
         """Calculates the area of a Rectangle instance
+
         Returns:
             Area of the the rectangle, given by height * width
         """
@@ -71,10 +75,10 @@ class Rectangle:
 
     def perimeter(self):
         """Calculates the perimeter of a Rectangle instance
+
         Returns:
             Perimeter of the rectangle, given by 2 * (height + width)
         """
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
