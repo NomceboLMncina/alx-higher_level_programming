9dbc71db225e739fbbc7c8663f0dbaef28e59c4b
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-10-15 03:50:14 -0700

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """Defines a file-writing function."""
 
+
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file.
+    """Write a string to a UTF8 text file.
 
     Args:
-        filename (str): The name of the file to write. If it
-        does not exist, it will be created.
+        filename (str): The name of the file to write.
         text (str): The text to write to the file.
-
     Returns:
-        int: The number of characters written.
+        The number of characters written.
     """
-    try:
-        with open(filename, "w", encoding="utf-8") as f:
-            return f.write(text)
-    except IOError as e:
-        print(f"An error occurred while writing to the file: {e}")
-        return 0
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
