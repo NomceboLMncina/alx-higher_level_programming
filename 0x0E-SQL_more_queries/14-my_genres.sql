f059a9c0180b0b09d10096fe49fd2f9950905858
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2024-11-20 10:20:16 -0800

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending genre name.
+SELECT z.`name`
+  FROM `tv_genres` AS z
+       INNER JOIN `tv_show_genres` AS s
+       ON z.`id` = y.`genre_id`
+
+       INNER JOIN `tv_shows` AS r
+       ON r.`id` = y.`show_id`
+       WHERE r.`title` = "Dexter"
+ ORDER BY z.`name`;
